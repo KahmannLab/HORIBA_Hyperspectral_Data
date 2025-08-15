5cab627d15b34f07a10cf9d736de21857e082a88
Mengru Sun <mengru.sun@physik.tu-chemnitz.de>
2025-08-15 18:07:49 +0200

    Update PCA.py

--- PCA.py
+++ PCA.py
@@ -4,8 +4,166 @@ import matplotlib.pyplot as plt
 from matplotlib_scalebar.scalebar import ScaleBar
 import matplotlib.ticker
 from matplotlib.ticker import MultipleLocator, AutoMinorLocator
+from mpl_toolkits.axes_grid1.anchored_artists import AnchoredSizeBar
 
-#%% Get the explained variance ratio plot (scree plot)
+#%% New: PCA from sklearn for hypserpy data
+# convert hyperspectral data to a 2D array where each row is a spectrum
+def stack_spectra_columnwise(cube):
+    """
+    Stack all spectra vertically into a 2D array.
+    Input: cube of shape (ny, nx, nspec)
+    Output: array of shape (nspec, ny*nx), column-by-column stacking
+    """
+    ny, nx, nspec = cube.shape
+    return cube.transpose(1, 0, 2).reshape(nx * ny, nspec)
+# convert the stacked spectra back to a 3D cube
+def unstack_spectra_columnwise(flat, ny, nx):
+    """
+    Restore stacked spectra back to a 3D cube (ny, nx, nspec)
+    Input: stacked array of shape (nspec, ny*nx)
+    """
+    nspec = flat.shape[1]
+    return flat.reshape(nx, ny, nspec).transpose(1, 0, 2)
+
+#%%
+from sklearn.decomposition import PCA
+
+def sklearn_PCA(data,ScreePlot=False,n_components=None,*args, **kwargs):
+    # flatten the datacube read by hyperspy
+    flat_data = stack_spectra_columnwise(data.data)
+    # apply PCA
+    pca = PCA(*args, **kwargs)
+    pca.fit(flat_data)
+    # save all components in an array
+    component_spectra = pca.components_
+    if ScreePlot:
+        # plot the explained variance ratio vs the number of components
+        EVR = pca.explained_variance_ratio_
+        N_components = np.arange(1, len(EVR) + 1)
+        if n_components is not None:
+            N_components = N_components[:n_components]
+            EVR = EVR[:n_components]
+        plt.plot(N_components, EVR, 'ro', markersize=5)
+        ''''
+        # using majorlocator and minor locator to set the x-axis ticks
+        plt.gca().xaxis.set_major_locator(MultipleLocator(2))
+        plt.gca().xaxis.set_minor_locator(AutoMinorLocator(1))
+        '''
+        plt.xticks(N_components, fontsize=10)
+        plt.yticks(fontsize=10)
+        plt.xlabel('Number of components')
+        plt.ylabel('Explained variance ratio')
+        plt.title('Explained variance ratio by PCA')
+        plt.tight_layout()
+        plt.show()
+
+    return pca, component_spectra
+
+#%% reconstruct the data
+def reconstruct_data(data, pca, component_idx=None, component_list=None):
+    """
+       Reconstruct data from PCA scores using a subset of components.
+
+       Parameters:
+           pca : trained sklearn PCA object
+           X_pca : array-like, shape (n_samples, n_components)
+               PCA-transformed data (scores)
+           n_first : int, optional
+               Number of first components to use (indices 0 to n_first-1)
+           component_list : list of int, optional
+               Specific component indices to use
+
+       Returns:
+           X_reconstructed : array-like, shape (n_samples, n_features)
+               Data reconstructed using selected components
+       """
+    flat_data = stack_spectra_columnwise(data.data)
+    data_pca = pca.transform(flat_data)
+    if component_idx is not None:
+        # Select first n_first components
+        selected_indices = list(range(component_idx))
+    elif component_list is not None:
+        # Select specific components
+        selected_indices = component_list
+    else:
+        raise ValueError("Either the index of component or component_list must be provided.")
+
+    # Extract scores and components
+    data_pca_sel = data_pca[:, selected_indices]
+    components_sel = pca.components_[selected_indices]
+
+    # Reconstruct data
+    data_reconstructed = np.dot(data_pca_sel, components_sel) + pca.mean_
+    datacube_reconstructed = unstack_spectra_columnwise(data_reconstructed, data.data.shape[0], data.data.shape[1])
+    return datacube_reconstructed
+
+#%% Score map
+# get the ideal scalebar length
+def get_scalebar_length(data, pixel_to_mum, percent=0.133335):
+    """
+    Calculate the ideal scalebar length based on the data and pixel size. Best length of a scale bar. 13% of the length of the image.
+    :param data: an image data
+    :param pixel_to_mum: Pixel size in micrometers.
+    :param percent: the percentage of the image length to be used for the scalebar, default is 0.133335 (13%).
+    :return:
+    len_in_pix (float): the length of the scalebar in pixels
+    length (float): the length of the scalebar in micrometers
+    width (float): the width of the scalebar in pixels
+    """
+    ideal_length_scale_bar = data.shape[1] *pixel_to_mum * percent  # 13% of the image length in micrometers
+
+    # Work out how many pixels are required for the scalebar. If scale bar length is > 10 round to the nearest 5.
+    if ideal_length_scale_bar > 10:
+        n = (ideal_length_scale_bar - 10) / 5
+        n = round(n)
+        length = int(10 + 5 * n)
+        len_in_pix = length / pixel_to_mum
+
+    # Round to the nearest integer if between 1 and 10.
+    elif (ideal_length_scale_bar <= 10) & (ideal_length_scale_bar >= 1):
+        n = int(round(ideal_length_scale_bar))
+        length = n
+        len_in_pix = length / pixel_to_mum
+
+    # Round to 1 decimal place if < 1.
+    elif ideal_length_scale_bar < 1:
+        n = round(ideal_length_scale_bar, 1)
+        length = n
+        len_in_pix = n / pixel_to_mum
+
+    width = 0.06 * len_in_pix
+
+    return len_in_pix, length, width
+def score_map(data, pca, component_index,*args, **kwargs):
+    """
+    Plot the score map of the given PC
+    :param data (LumiSpectrum): hyperspectral data
+    :param pca: trained sklearn PCA object
+    :param component_index (int): the index of the principal component starting from 1
+    """
+    flat_data = stack_spectra_columnwise(data.data)
+    data_pca = pca.transform(flat_data)
+    # reshape the PCA scores to match the original data shape
+    data_pca_reshaped = unstack_spectra_columnwise(data_pca, data.data.shape[0], data.data.shape[1])
+    score_map = data_pca_reshaped[:,:, component_index - 1]  # component_index is starting from 1, so we need to subtract 1 for zero-based indexing
+    # Get the scalebar length
+    len_in_pix, length, width = get_scalebar_length(score_map, data.axes_manager[0].scale, *args, **kwargs)
+
+    fig, ax = plt.subplots()
+    cmap = ax.imshow(score_map, cmap='viridis')
+    ax.set_axis_off()
+    scalebar = AnchoredSizeBar(ax.transData, len_in_pix, str(length) + ' μm', 4, pad=1,
+                                borderpad=0.1, sep=5, frameon=False, size_vertical=width, color='white',
+                                fontproperties={'size': 15, 'weight': 'bold'})
+    ax.add_artist(scalebar)
+    cbar = fig.colorbar(cmap, ax=ax)
+    cbar.set_label('Score')
+    ax.set_title('Score map of the {}. PC'.format(component_index))  # index shown in the title is starting from 1
+    plt.tight_layout()
+    plt.show()
+'''    
+#%% Old PCA functions based on Hyperspy PCA
+# Get the explained variance ratio plot (scree plot)
 def plot_evr(dataPCA, n_components,save_path=None):
     """
     Plot the explained variance ratio by PCA
@@ -69,13 +227,13 @@ def PC_spc(dataPCA,component_index, data_type,major_locator=50,n_minor_locator=2
 
 #%% Plot the loading map of the given PC
 def loading_map(dataPCA, component_index,ROI=None,save_path=None):
-    '''
+    """
     Plot the loading map of the given PC
     :param dataPCA: the data after PCA decomposition
     :param component_index: the index of the principal component starting from 1
     :param ROI(list): the region of interest to be shown in the loading map[y,y+dy,x,x+dx]
     :return:
-    '''
+    """
     loading_maps = dataPCA.get_decomposition_loadings()
     loading_map = loading_maps.data[component_index-1,:,:]
     if ROI is not None:
@@ -164,3 +322,4 @@ def rec_spc_animation(dataPCA, px, start_nPCs, end_nPCs=1, save_path='C:/animati
 
     ani = FuncAnimation(fig, update_plot, frames=len(rec_spcs), repeat=False)
     ani.save(save_path, writer=writer, fps=fps)
+'''
